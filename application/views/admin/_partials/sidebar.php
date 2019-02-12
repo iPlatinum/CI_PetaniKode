5359fd05403304de9e05b919b12685e5db4f7896
Maulana <dmh_maulana1406@yahoo.com>
2019-02-12 13:57:24 +0700

    CI_PetaniCode

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -17,6 +17,19 @@
             <a class="dropdown-item" href="<?php echo site_url('admin/products') ?>">List Product</a>
         </div>
     </li>
+
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'supplier' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>Supplier</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="<?php echo site_url('admin/supplier/add') ?>">New Supplier</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/supplier') ?>">List Supplier</a>
+        </div>
+    </li>
+
     <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'customer' ? 'active': '' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false">
